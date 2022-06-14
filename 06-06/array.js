63e5d963196065884a0e2a01ce6c9c507364c270
FABIANA ESTELA DA SILVA YAMANA <44611897+estelayamana@users.noreply.github.com>
2022-06-14 15:47:01 -0300

    Update array.js

--- 06-06/array.js
+++ 06-06/array.js
@@ -1,2 +1,18 @@
+// Criando um array:
 const marcaCarros = ["Ford", "Hyundai", "Fiat", "BMW", "Etios"];
-console.log("marcaCarros[2]")
+
+// Adicionando elementos na ultima posição do array
+marcaCarros[marcaCarros.length - 1] = "LisCar";
+marcaCarros.push("Honda");
+// console.log(marcaCarros);
+// console.log(marcaCarros.sort());
+
+/* 
+    Aprendendo sobre o splice
+*/
+const massas = ["Lasanha", "Pizza", "Macarronada"]
+console.log(massas);
+
+// ["Lasanha", "Nhoque", "Pizza", "Macarronada"]
+massas.splice(1, 0, "Nhoque");
+console.log(massas);
